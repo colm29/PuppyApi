77f2bca8bd5057f7721a14854445c98c35c0faf1
Colm <colm29@gmail.com>
2019-10-17 21:36:36 +0100

    Different request types

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-# Create the appropriate app.route functions. Test and see if they work
+# Create the appropriate app.route functions, test and see if they work, and paste your URIs in the boxes below.
 
-# Make an app.route() decorator here for when the client sends the URI "/puppies"
-@app.route('/puppies')
+# Make an app.route() decorator here
+
+
+@app.route('/puppies', methods={'GET', 'POST'})
 def puppiesFunction():
-    return "Yes, puppies!"
+    if request.method == 'GET':
+        # Call the method to Get all of the puppies
+        return getAllPuppies()
+    elif request.method == 'POST':
+        # Call the method to make a new puppy
+        return makeANewPuppy()
+        # Make another app.route() decorator here that takes in an integer id in the
 
 
-# Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
-@app.route('/puppies/<int:id>')
+@app.route('/puppies/<int:id>', methods={'GET', 'PUT', 'DELETE'})
 def puppiesFunctionId(id):
-    return "This method will act on the puppy with id %s" % id
+    if request.method == 'GET':
+        return getPuppy(id)
+
+    if request.method == 'PUT':
+        return updatePuppy(id)
+
+    elif request.method == 'DELETE':
+        return deletePuppy(id)
+
+
+def getAllPuppies():
+    return "Getting All the puppies!"
+
+
+def makeANewPuppy():
+    return "Creating A New Puppy!"
+
+
+def getPuppy(id):
+    return "Getting Puppy with id %s" % id
+
+
+def updatePuppy(id):
+    return "Updating a Puppy with id %s" % id
+
+
+def deletePuppy(id):
+    return "Removing Puppy with id %s" % id
 
 
 if __name__ == '__main__':
