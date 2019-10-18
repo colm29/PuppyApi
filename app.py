9a96d67885f9589b6c016e5e345cd5da659b9a84
Colm <colm29@gmail.com>
2019-10-18 10:15:33 +0100

    Add model and serializer

--- app.py
+++ app.py
@@ -1,53 +1,109 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Puppy
+
+
+engine = create_engine('sqlite:///puppies.db')
+Base.metadata.bind = engine
+
 app = Flask(__name__)
-# Create the appropriate app.route functions, test and see if they work, and paste your URIs in the boxes below.
-
-# Make an app.route() decorator here
 
 
-@app.route('/puppies', methods={'GET', 'POST'})
+def newSession():
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
+
+
+@app.route("/")
+@app.route("/puppies", methods=['GET', 'POST'])
 def puppiesFunction():
     if request.method == 'GET':
-        # Call the method to Get all of the puppies
         return getAllPuppies()
     elif request.method == 'POST':
-        # Call the method to make a new puppy
-        return makeANewPuppy()
-        # Make another app.route() decorator here that takes in an integer id in the
+        print("Making a New puppy")
+
+        name = request.args.get('name', '')
+        description = request.args.get('description', '')
+        print(name)
+        print(description)
+        return makeANewPuppy(name, description)
 
 
-@app.route('/puppies/<int:id>', methods={'GET', 'PUT', 'DELETE'})
+# Make another app.route() decorator here that takes in an integer id in the URI
+@app.route("/puppies/<int:id>", methods=['GET', 'PUT', 'DELETE'])
+# Call the method to view a specific puppy
 def puppiesFunctionId(id):
     if request.method == 'GET':
         return getPuppy(id)
 
-    if request.method == 'PUT':
-        return updatePuppy(id)
+# Call the method to edit a specific puppy
+    elif request.method == 'PUT':
+        name = request.args.get('name', '')
+        description = request.args.get('description', '')
+        return updatePuppy(id, name, description)
 
+     # Call the method to remove a puppy
     elif request.method == 'DELETE':
         return deletePuppy(id)
 
 
 def getAllPuppies():
-    return "Getting All the puppies!"
-
-
-def makeANewPuppy():
-    return "Creating A New Puppy!"
+    session = newSession()
+    try:
+        puppies = session.query(Puppy).all()
+        return jsonify(Puppies=[i.serialize for i in puppies])
+    finally:
+        session.close()
 
 
 def getPuppy(id):
-    return "Getting Puppy with id %s" % id
+    session = newSession()
+    try:
+        puppy = session.query(Puppy).filter_by(id=id).one()
+        return jsonify(puppy=puppy.serialize)
+    finally:
+        session.close()
 
 
-def updatePuppy(id):
-    return "Updating a Puppy with id %s" % id
+def makeANewPuppy(name, description):
+    session = newSession()
+    try:
+        puppy = Puppy(name=name, description=description)
+        session.add(puppy)
+        session.commit()
+        return jsonify(Puppy=puppy.serialize)
+    finally:
+        session.close()
+
+
+def updatePuppy(id, name, description):
+    session = newSession()
+    try:
+        puppy = session.query(Puppy).filter_by(id=id).one()
+        if not name:
+            puppy.name = name
+        if not description:
+            puppy.description = description
+        session.add(puppy)
+        session.commit()
+        return "Updated a Puppy with id %s" % id
+    finally:
+        session.close()
 
 
 def deletePuppy(id):
-    return "Removing Puppy with id %s" % id
+    session = newSession()
+    try:
+        puppy = session.query(Puppy).filter_by(id=id).one()
+        session.delete(puppy)
+        session.commit()
+        return "Removed Puppy with id %s" % id
+    finally:
+        session.close()
 
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False
     app.run(host='0.0.0.0', port=5000)
--- endpoints_tester.py
+++ endpoints_tester.py
@@ -7,6 +7,28 @@ address = input(
     "Please enter the address of the server you want to access, \n If left blank the connection will be set to 'http://localhost:5000':   ")
 if address == '':
     address = 'http://localhost:5000'
+
+
+# Making a POST Request
+print("Making a POST request to /puppies...")
+try:
+    url = address + "/puppies?name=Fido&description=Playful+Little+Puppy"
+    h = httplib2.Http()
+    resp, result = h.request(url, 'POST')
+    obj = json.loads(result)
+    puppyID = obj['Puppy']['id']
+    if resp['status'] != '200':
+        raise Exception(
+            'Received an unsuccessful status code of %s' % resp['status'])
+
+except Exception as err:
+    print("Test 1 FAILED: Could not make POST Request to web server")
+    print(err.args)
+    sys.exit()
+else:
+    print("Test 1 PASS: Succesfully Made POST Request to /puppies")
+
+
 # Making a GET Request
 print("Making a GET Request for /puppies...")
 try:
@@ -17,44 +39,25 @@ try:
         raise Exception(
             'Received an unsuccessful status code of %s' % resp['status'])
 except Exception as err:
-    print("Test 1 FAILED: Could not make GET Request to web server")
+    print("Test 2 FAILED: Could not make GET Request to web server")
     print(err.args)
     sys.exit()
 else:
-    print("Test 1 PASS: Succesfully Made GET Request to /puppies")
-
-
-# Making a POST Request
-print("Making a POST request to /puppies...")
-try:
-    url = address + "/puppies"
-    h = httplib2.Http()
-    resp, result = h.request(url, 'POST')
-    if resp['status'] != '200':
-        raise Exception(
-            'Received an unsuccessful status code of %s' % resp['status'])
-
-except Exception as err:
-    print("Test 2 FAILED: Could not make POST Request to web server")
-    print(err.args)
-    sys.exit()
-else:
-    print("Test 2 PASS: Succesfully Made POST Request to /puppies")
+    print("Test 2 PASS: Succesfully Made GET Request to /puppies")
 
 
 # Making GET Requests to /puppies/id
 print("Making GET requests to /puppies/id ")
 
 try:
-    id = 1
-    while id <= 10:
-        url = address + "/puppies/%s" % id
-        h = httplib2.Http()
-        resp, result = h.request(url, 'GET')
-        if resp['status'] != '200':
-            raise Exception(
-                'Received an unsuccessful status code of %s' % resp['status'])
-        id = id + 1
+    id = puppyID
+    url = address + "/puppies/%s" % id
+    h = httplib2.Http()
+    resp, result = h.request(url, 'GET')
+    if resp['status'] != '200':
+        raise Exception(
+            'Received an unsuccessful status code of %s' % resp['status'])
+
 
 except Exception as err:
     print("Test 3 FAILED: Could not make GET Requests to web server")
@@ -68,15 +71,14 @@ else:
 print("Making PUT requests to /puppies/id ")
 
 try:
-    id = 1
-    while id <= 10:
-        url = address + "/puppies/%s" % id
-        h = httplib2.Http()
-        resp, result = h.request(url, 'PUT')
-        if resp['status'] != '200':
-            raise Exception(
-                'Received an unsuccessful status code of %s' % resp['status'])
-        id = id + 1
+    id = puppyID
+
+    url = address + "/puppies/%s?name=wilma&description=A+sleepy+bundle+of+joy" % id
+    h = httplib2.Http()
+    resp, result = h.request(url, 'PUT')
+    if resp['status'] != '200':
+        raise Exception(
+            'Received an unsuccessful status code of %s' % resp['status'])
 
 except Exception as err:
     print("Test 4 FAILED: Could not make PUT Request to web server")
@@ -91,15 +93,14 @@ else:
 print("Making DELETE requests to /puppies/id ... ")
 
 try:
-    id = 1
-    while id <= 10:
-        url = address + "/puppies/%s" % id
-        h = httplib2.Http()
-        resp, result = h.request(url, 'DELETE')
-        if resp['status'] != '200':
-            raise Exception(
-                'Received an unsuccessful status code of %s' % resp['status'])
-        id = id + 1
+    id = puppyID
+    url = address + "/puppies/%s" % id
+    h = httplib2.Http()
+    resp, result = h.request(url, 'DELETE')
+    if resp['status'] != '200':
+        raise Exception(
+            'Received an unsuccessful status code of %s' % resp['status'])
+
 
 except Exception as err:
     print("Test 5 FAILED: Could not make DELETE Requests to web server")
